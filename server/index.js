d721fa158c05931960209bc5957e679777f989ad
Yakob72119 <beshayakob72119@gmail.com>
2024-03-11 11:22:26 +0300

    server side code for registering a student

--- server/index.js
+++ server/index.js
@@ -1,11 +1,72 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect('mongodb://localhost:27017/erasen_lewaq_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  department: String,
+  gender: String,
+  university: String,
+  results: [{
+    result: String
+  }],
+  user: {
+    username: String,
+    password: String,
+  }
+});
+
+
+const Student = mongoose.model('Student', studentSchema);
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+app.use(express.json());
+
+
+app.post('/api/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ msg: 'Username already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ msg: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
