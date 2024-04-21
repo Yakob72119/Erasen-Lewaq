83cded284ed4432697304919cd4e7fe51ac00093
Alfeyn <alifdesigns90@gmail.com>
2024-04-21 14:07:31 +0300

    some back end fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import './App.scss';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Registration from './page/Registration';
 import Index from './page/Index';
 import Login from './page/Login';
@@ -10,40 +10,72 @@ import EduDashboard from './page/EduDashboard';
 import AdminDashboard from './page/AdminDashboard';
 import StudDashboard from './page/StudDashboard';
 import StudProfile from './page/StudProfile';
+import AdminProfile from './page/AdminProfile';
 import EduProfile from './page/EduProfile';
 
 const ProtectedRoute = ({ role, element, ...rest }) => {
-  useEffect(() => {
-    const isAuthenticated = /* Retrieve isAuthenticated from session */;
-    const userRole = /* Retrieve userRole from session */;
-    if (!isAuthenticated || (role !== userRole && role !== 'admin')) {
-      // Redirect to login page if not authenticated or role does not match
-      window.location.href = '/login';
-    }
-  }, [role]);
+  const navigate = useNavigate(); // Get the navigation function
 
-  return <Route {...rest} element={element} />;
+  useEffect(() => {
+    const isAuthenticated = sessionStorage.getItem('isAuthenticated');
+    const userRole = sessionStorage.getItem('role');
+    if (!isAuthenticated || (role !== userRole && role !== 'admin')) {
+      navigate('/login');
+    }
+  }, [role, navigate]);
+
+  return role;
+};
+
+ProtectedRoute.propTypes = {
+  role: PropTypes.string.isRequired,
+  element: PropTypes.element.isRequired
 };
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
+        <Routes path="/">
+          {/* Protected routes for educator */}
+          <Route path="educator-dashboard" role="educator" element={
+            <ProtectedRoute>
+              <EduDashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="educator-profile" role="educator" element={
+            <ProtectedRoute>
+              <EduProfile />
+            </ProtectedRoute>
+          } />
+          {/* Protected routes for admin */}
+          <Route path="admin-dashboard" role="admin" element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="admin-profile" role="admin" element={
+            <ProtectedRoute>
+              <AdminProfile />
+            </ProtectedRoute>
+          } />
+          {/* Protected routes for student */}
+          <Route path="student-dashboard" role="student" element={
+            <ProtectedRoute>
+              <StudDashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="student-profile" role="student" element={
+            <ProtectedRoute>
+              <StudProfile />
+            </ProtectedRoute>
+          } />
           <Route index element={<Index />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Registration />} />
           <Route path="information" element={<RegisterInfo />} />
           <Route path="educator-registration" element={<EducatorRegister />} />
-          {/* Protected routes for educator */}
-          <ProtectedRoute path="educator-dashboard" role="educator" element={<EduDashboard />} />
-          <ProtectedRoute path="educator-profile" role="educator" element={<EduProfile />} />
-          {/* Protected routes for admin */}
-          <ProtectedRoute path="admin-dashboard" role="admin" element={<AdminDashboard />} />
-          <ProtectedRoute path="admin-profile" role="admin" element={<AdminProfile />} />
-          {/* Protected routes for student */}
-          <ProtectedRoute path="student-dashboard" role="student" element={<StudDashboard />} />
-          <ProtectedRoute path="student-profile" role="student" element={<StudProfile />} />
+
         </Routes>
       </BrowserRouter>
     </div>
