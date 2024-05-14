7626c0ea40ddf74716fac8bf0a31bed6e7cdf84a
Yakob Beshahwered <beshayakob72119@gmail.com>
2024-05-14 15:05:31 +0300

    student buying coin.done

--- client/src/components/StudentWalletComponent.jsx
+++ client/src/components/StudentWalletComponent.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import coinb from '../assets/coin.svg'
-import coins from '../assets/coins.svg'
-import wallet from '../assets/wallet-svg.svg'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import coinb from '../assets/coin.svg';
+import coins from '../assets/coins.svg';
+import wallet from '../assets/wallet-svg.svg';
 
 const StudentWalletComponent = () => {
-  const [coin, setCoin] = useState('')
+  const [coin, setCoin] = useState('');
   const [submit, setSubmit] = useState(false);
-  const [deleteCurrent, setDeleteCurrent] = useState('hide')
+  const [deleteCurrent, setDeleteCurrent] = useState('hide');
+  const [paymentVerified, setPaymentVerified] = useState(false);
 
+ 
+  
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setSubmit(false)
+      setSubmit(false);
     }, 5000);
 
     return () => clearTimeout(timer);
@@ -19,71 +24,95 @@ const StudentWalletComponent = () => {
 
   const handleInputChange = (event) => {
     const { value } = event.target;
-    if (parseFloat(value) > 0 || value === "") {
+    if (parseFloat(value) >= 0 || value === '') {
       setCoin(value);
     }
   };
 
   const handleBuyOpen = () => {
-
     if (coin !== '') {
       setDeleteCurrent('show');
       setSubmit(false);
-
     } else {
       setSubmit(true);
     }
-
-  }
+  };
 
   const handleBuyClose = () => {
     setDeleteCurrent('hide');
-  }
+  };
+
+  const handleBuySubmit = () => {
+    // Assuming you have a function to get the user ID from the session
+    const studentId = sessionStorage.getItem('_id');
+
+    // Assuming you have an API endpoint for buying coins
+    const apiUrl = "http://localhost:3000/payment/buycoin"
+
+    // Make a POST request to your backend API using Axios
+    axios.post(apiUrl, {
+      userId: studentId,
+      amount: parseFloat(coin),
+    })
+      .then((response) => {
+        // Handle response from backend
+        const checkoutUrl = response.data.checkout_url;
+      window.open(checkoutUrl, '_blank');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
   return (
     <div className='wallet'>
-      <img className='background' src={coinb} alt="" />
-      <div className="header">
-        <div className="title">
-          <img src={wallet} alt="" />
+      <img className='background' src={coinb} alt='' />
+      <div className='header'>
+        <div className='title'>
+          <img src={wallet} alt='' />
           <h1>Wallet</h1>
         </div>
-        <div className="disc">! 1 Erasen-lewaq coin = 1 ETB</div>
+        <div className='disc'>! 1 Erasen-lewaq coin = 1 ETB</div>
       </div>
-      <div className="body">
-        <div className="balance">
-          <div className="view">
+      <div className='body'>
+        <div className='balance'>
+          <div className='view'>
             <h1>2000 Coin</h1>
             <p>Total balance</p>
           </div>
-          <img src={coins} alt="" />
+          <img src={coins} alt='' />
         </div>
-        <div className="buy">
+        <div className='buy'>
           <div className='buy-input'>
             <input
-              type="number"
-              name="coin"
-              className="input"
-              placeholder="Coin"
+              type='number'
+              name='coin'
+              className='input'
+              placeholder='Coin'
               value={coin}
               onChange={handleInputChange}
               {...(submit && coin === '' && { required: true })}
             />
-            <button onClick={handleBuyOpen} className='buy-btn'>Buy</button>
+            <button onClick={handleBuyOpen} className='buy-btn'>
+              Buy
+            </button>
           </div>
         </div>
       </div>
       <div className={`add-new-admin ${deleteCurrent}`}>
-        <button onClick={handleBuyClose} className='close'>Close</button>
+        <button onClick={handleBuyClose} className='close'>
+          Close
+        </button>
 
-        <div className="newAdminForm">
-          <p>Are You sure you want to Buy 200Coins?</p>
+        <div className='newAdminForm'>
+          <p>Are You sure you want to Buy {coin} Coins?</p>
         </div>
-        <input type="submit" value="Buy" className="buyBtn" />
-
+        <button onClick={handleBuySubmit} className='buyBtn'>
+          Buy
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StudentWalletComponent
+export default StudentWalletComponent;
